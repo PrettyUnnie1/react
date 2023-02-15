191ee2b4f7c970db50634bd0ae7651507fe4a5c1
PrettyUnnnie1 <mrbin12192003@gmail.com>
2023-02-15 20:23:35 +0700

    ahihi

---
+++ leagueoflegends-build-youtube/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCjGjaD5uwg98n0VlODcFlCUz14ceibRyU",
+    authDomain: "leagueoflegends-clone.firebaseapp.com",
+    projectId: "leagueoflegends-clone",
+    storageBucket: "leagueoflegends-clone.appspot.com",
+    messagingSenderId: "478760046999",
+    appId: "1:478760046999:web:bdbb6788717fe0559634d2"
+  };
+  
